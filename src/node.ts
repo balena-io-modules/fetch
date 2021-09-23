8f8d75941a491e4b773968419102c69d493aa305
Zane Hitchcox <zane@balena.io>
2021-09-23 16:03:30 -0400

    test node fetch

--- src/node.ts
+++ src/node.ts
@@ -1,2 +1,21 @@
+import { Server } from 'http';
 import fetch from 'node-fetch'
+import { createTestServer, expect, test } from '../test/test';
 export default fetch
+
+test('node fetch', async () => {
+  let server: Server, port: number;
+  test('setup', async () => {
+    ({server, port} = await createTestServer());
+  });
+
+  test('can browser fetch', async () => {
+    const resp = await fetch(`http://localhost:${port}`);
+    expect(resp.status).toBe(200);
+    expect(await resp.text()).toBe('It works!')
+  });
+
+  test('teardown', () => {
+    server.close();
+  });
+})
