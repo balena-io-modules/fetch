76696b8ee93d5592eb0858218a389023d61e0ebb
Zane Hitchcox <zane@balena.io>
2021-09-24 12:35:51 -0400

    remove comments

--- src/he-connect.ts
+++ src/he-connect.ts
@@ -35,33 +35,17 @@ export async function heConnect(url: URL, cb: (err: Error | undefined, socket?: 
   const port = url.port.length ? Number(url.port) : (protocol === "https:" ? 443 : 80);
   const addrs = Array.from(await getAddrInfo(url.hostname));
 
-  // const v4 = lookups.filter(lookup => lookup.family === 4);
-  // const v6 = lookups.filter(lookup => lookup.family === 6);
-  // const _lastUsed = lastUsed[hostname] ?? lastUsedPeriod;
-  // const addrs = _lastUsed === 0 ? lookups :
-
-  // for (let i = 0; i < Math.max(v4.length,v6.length); i++) {
-  //   addrs.push((_lastUsed === 0 ? lookups : _lastUsed === 4 ? v4[i] : v6[i]).address);
-  // }
-
-
-  // // (_lastUsed === 0 ? lookups : _lastUsed === 4 ? [...v4, ...v6] : [...v6, ...v4]).map(lookup => lookup.address);
-  // if (addrs.length < 1) {
-  //   throw new Error(`Could not resolve host, ${hostname}`)
-  // }
-
   let err: Error;
   const sockets: net.Socket[] = [];
   let ctFound = false;
   let failed = 0;
   let i = 0;
-  // @ts-ignore
   for (const host of addrs) {
     if (ctFound) {
       break;
     }
     debug(`Trying ${host}...`);
-    // @ts-ignore the options are thesame for {net,tls}.connect, except for servername
+    // @ts-ignore the options are the same for {net,tls}.connect, except for servername
     // which gets ignored by net.connect
     const socket = (protocol === 'https:' ? tls : net).connect({
       host,
